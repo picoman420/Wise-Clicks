d38cde93db1ed563872e8692f23cf528f9797b66
manujsingh420420420 <162284919+manujsingh420420420@users.noreply.github.com>
2025-02-11 15:57:33 +0800

    Added Back button functionality in level 1

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -10,4 +10,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(1);
     }
+        public void BackToPreviousScene()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
 }
