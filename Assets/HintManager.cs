e543e824cede322a7db3088dab3a447652317988
manujsingh420420420 <162284919+manujsingh420420420@users.noreply.github.com>
2025-03-06 10:53:54 +0800

    Added the dynamic hints functionality

--- Assets/HintManager.cs
+++ Assets/HintManager.cs
@@ -2,26 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HintManager : MonoBehaviour
 {
-    public GameObject[] messagePanels; // Assign Msg1Panel, Msg2Panel, etc.
+    public GameObject[] messagePanels;
+    public TextMeshProUGUI hintText; // Assign Msg1Panel, Msg2Panel, etc.
     public string[] hints; // Assign hints for each panel
-    public Text hintText; // Assign the Text in HintPanel
+
     public GameObject hintPanel; // Assign the HintPanel
 
     public void ShowHint()
-    {
+    {   
+        bool hintFound = false;
         for (int i = 0; i < messagePanels.Length; i++)
         {
             if (messagePanels[i].activeSelf) // Check which panel is active
             {
-                hintText.text = hints[i];
-                hintPanel.SetActive(true); // Show hint panel
-                return;
+                hintText.text = hints[i]; // Update hint text
+                hintFound = true;
+                break;
             }
         }
-        hintPanel.SetActive(false); // Hide hint if no panel is active
+        hintPanel.SetActive(hintFound); // Hide hint if no panel is active
     }
     // Start is called before the first frame update
     void Start()
