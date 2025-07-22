fdc02f50e016936a3fc634642cec4778a9b49d3c
Qnweii <QWONG011@e.ntu.edu.sg>
2025-07-23 01:22:05 +0800

    setup UI for code implementation

---
+++ Assets/ReusedScripts/QuizManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    // private variables
+    private int randomNum;
+    private int totalQns = 7;
+    private int tracker = 0;
+
+    private List<int> currentRandNums = new List<int>();
+
+
+    // public variables
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Ensure new random number produces, not duplicated
+    public void RandomNumGenerator()
+    {
+        
+    }
+}
